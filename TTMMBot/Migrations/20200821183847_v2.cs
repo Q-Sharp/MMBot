d303b97872666441dadce18b8cbbf53d55a8128b
QoOdrink3r <qoodrink3r@outlook.de>
2020-08-21 20:42:01 +0200

    Add missing migration

---
+++ TTMMBot/Migrations/20200821183847_v2.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TTMMBot.Migrations
+{
+    public partial class v2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AllTimeHigh",
+                table: "Member",
+                newName: "AHigh");
+
+            migrationBuilder.RenameColumn(
+                name: "JoinOrder",
+                table: "Member",
+                newName: "Join");
+
+            migrationBuilder.RenameColumn(
+                name: "SeasonHighest",
+                table: "Member",
+                newName: "SHigh");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AHigh",
+                table: "Member",
+                newName: "AllTimeHigh");
+
+            migrationBuilder.RenameColumn(
+                name: "Join",
+                table: "Member",
+                newName: "JoinOrder");
+
+            migrationBuilder.RenameColumn(
+                name: "SHigh",
+                table: "Member",
+                newName: "SeasonHighest");
+        }
+    }
+}
