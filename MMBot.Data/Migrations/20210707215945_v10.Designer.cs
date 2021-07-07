f682ebeb9fe024ccb6a665de2b6158a99a1b57f0
QoOdrink3r <qoodrink3r@outlook.de>
2021-07-08 00:00:09 +0200

    Update datamodel

--- MMBot.Data/Migrations/20210707215945_v10.Designer.cs
+++ MMBot.Data/Migrations/20210707215945_v10.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using MMBot.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace MMBot.Data.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210707215945_v10")]
+    partial class v10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MMBot.Data/Migrations/20210707215945_v10.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MMBot.Data.Migrations
+{
+    public partial class v10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutoSignUpForFightNight",
+                table: "Member");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutoSignUpForFightNight",
+                table: "Member",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
